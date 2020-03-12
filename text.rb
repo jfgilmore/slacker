1ef0480c913594959155a7389881807768dcffa9
Joshua Gilmore <djsounddog@gmail.com>
2020-03-12 18:54:11 +1100

    Added file read/write to text.rb for testing of file source to encrypt and decrypt to. Not yet functional.

--- text.rb
+++ text.rb
@@ -1,33 +1,26 @@
 require 'encryptor'
 require 'securerandom'
-
-def decrypt encrypted_value
   cipher = OpenSSL::Cipher.new('aes-256-gcm')
   # cipher.encrypt # Required before '#random_key' or '#random_iv' can be called. http://ruby-doc.org/stdlib-2.0.0/libdoc/openssl/rdoc/OpenSSL/Cipher.html#method-i-encrypt
   secret_key = SecureRandom.random_bytes(32) # Insures that the key is the correct length respective to the algorithm used.
   iv = SecureRandom.random_bytes(16) # Insures that the IV is the correct length respective to the algorithm used.
-  # salt = SecureRandom.random_bytes(16)
-
   Encryptor.default_options.merge!(algorithm: 'aes-256-cbc', key: secret_key, iv: iv)
+
+def decrypt encrypted_value
   decrypted_value = Encryptor.decrypt(value: encrypted_value)
   decrypted_value
 end
 
 def encrypt string
-  cipher = OpenSSL::Cipher.new('aes-256-gcm')
-  # cipher.encrypt # Required before '#random_key' or '#random_iv' can be called. http://ruby-doc.org/stdlib-2.0.0/libdoc/openssl/rdoc/OpenSSL/Cipher.html#method-i-encrypt
-  secret_key = SecureRandom.random_bytes(32) # Insures that the key is the correct length respective to the algorithm used.
-  iv = SecureRandom.random_bytes(16) # Insures that the IV is the correct length respective to the algorithm used.
-  # salt = SecureRandom.random_bytes(16)
-  
-  Encryptor.default_options.merge!(algorithm: 'aes-256-cbc', key: secret_key, iv: iv)
   encrypted_value = Encryptor.encrypt(value: string)
   encrypted_value
 end
 
-string = "hello, world"
-string = encrypt string
-puts string
+x = 0
+keys = []
+File.foreach(File.expand_path('config/test', __dir__)) { |line| keys[x] = line.chomp; x += 1 }
 
-string = decrypt string
-puts string
+p keys
+
+File.write(File.expand_path('config/test', __dir__), encrypt keys[0,1,2].join("\n"))
+file.close
