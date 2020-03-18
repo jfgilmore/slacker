e0a8f3bf088b4f13ef0d8c3e12892e32fef0f3af
Joshua Gilmore <djsounddog@gmail.com>
2020-03-18 14:51:54 +1100

    Chenged library file na,es to meet convention, removed assignment submission directory, and restored terminal clear command functions in index.rb. Refactored code so required libraries in headder.

---
+++ index.rb
@@ -0,0 +1,117 @@
+#!/Users/gilmore/.rbenv/shims/ruby
+# frozen_string_literal=true
+
+system("clear")
+# On first run generate config files and run bundle install
+unless File.exist? File.expand_path('config', __dir__)
+  puts 'generating config files...'
+  `mkdir #{File.expand_path('config', __dir__)}`
+  `chmod -R 0400 config`
+  `bundle install` # Install any missing gems
+end
+
+quick = false
+quick = true if ARGV.include? '-q'
+if ARGV.include? '-uninstall'
+  # remove application and encryption keys & secret codes
+end
+if ARGV.include?('-v') || ARGV.include?('man')
+  # show version
+  file = File.read File.expand_path('README.md', __dir__)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  if ARGV.include? '-v'
+    file.each_line do |l|
+      if l.match(/version:....../)
+        puts l.match(/version:....../)
+        break
+      end
+    end
+  end
+end
+if ARGV.include? 'man'
+  puts markdown.render file
+  exit
+end
+
+require 'bundler/setup'
+Bundler.require(:default)
+require 'rubygems'
+require 'artii'
+require 'colorize'
+require 'colorized_string'
+require 'tty-prompt'
+require 'redcarpet'
+require 'redcarpet/render_strip'
+require_relative 'lib/slack'
+require_relative 'lib/local_server'
+
+# Print closing tag
+def close
+  system("clear")
+  line
+  puts "Thanks for using______\n#{$art.asciify('Slacker').colorize(:green)}"
+  line
+  exit
+end
+
+def line
+  puts '-' * 40
+end
+
+prompt = TTY::Prompt.new
+$art = Artii::Base.new
+
+# Opening tag
+line
+puts $art.asciify('Slacker').colorize(:green)
+line
+
+# Check internet connection available
+def online
+  check = LocalServer.new
+  check.get 'https://google.com'
+end
+
+online
+# If the quick login argument given, skip the login y/n prompt
+if quick
+  slack = Slack.new
+else
+  # Prompt user to sign in.
+  prompt.yes?('Do you want to login to Slack?') ? slack = Slack.new : close
+  # need timout error handling :(
+end
+puts 'Login error, please try again' unless slack.login
+
+previous = :ch
+type = slack.channels
+loop do
+  while slack.conversation == :pm || slack.conversation == :ch ||
+        !slack.conversation
+    system("clear")
+    previous = slack.conversation
+    slack.conversation = prompt.select 'Select a private message thread:', type
+    case slack.conversation
+    when :ch
+      type = slack.channels
+    when :pm
+      type = slack.users
+    when false
+      close
+    end
+  end
+
+  # Get History, print
+  # slack.history
+
+  # Begin a chat session
+  puts 'Leave message blank to change conversation'
+  chat = true
+  while chat
+    print "#{slack.conversation_name}:"
+    msg = gets.chomp
+    chat = slack.message msg
+    'Message undelivered: check your internet connection' unless chat == true
+  end
+  slack.conversation = previous
+end
